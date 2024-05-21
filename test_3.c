72e8f822477ebebaa3fce9a60a89dac10c2f5117
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-21 12:10:09 +0100

    task is done

--- test_3.c
+++ test_3.c
@@ -6,7 +6,7 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:31:53 by ymomen            #+#    #+#             */
-/*   Updated: 2024/05/19 11:45:48 by ymomen           ###   ########.fr       */
+/*   Updated: 2024/05/20 19:37:50 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned long int x = 0;
 pthread_mutex_t mutix;
-void *rotine()
+int d[]= {1,2,3,4,5,6,7,8,9,10};
+void *rotine(void *arg)
 {
-	long int i = 0;
-	pthread_mutex_lock(&mutix);
-	while(i++ < 100000000)
-		x++;
-	pthread_mutex_unlock(&mutix);
+	sleep(1);
+	int i = *(int *)arg;
+	printf("%d\n",d[i]);
+	free(arg);
 	return (NULL);
 }
 int	main(int ac, char **av)
 {
 	(void)av;
 	pthread_t t1[ac];
-	srand(time(NULL));
 	pthread_mutex_init(&mutix, NULL);
-	
 	int i = 0;
 	while(i < ac)
 	{
-		if (pthread_create(&t1[i++], NULL, &rotine, NULL ) != 0)
+		int *ptr = malloc(4);
+		*ptr = i;
+		if (pthread_create(&t1[i], NULL, &rotine, ptr) != 0)
 			return (1);
-		printf("the pthread %d start\n", i );
+		i++;
 	}
 	i = 0;
 	while(i < ac)
 	{
 		if ((pthread_join(t1[i++], NULL)))
 			return (6);
-		printf("the pthread %d finish\n", i);
 	}
 	if(pthread_mutex_destroy(&mutix))
 		return (5);
-	printf("%lu\n", x);
 	return (0);
 }
