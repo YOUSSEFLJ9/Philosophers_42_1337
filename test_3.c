11d694eb63f06b4b614732ddcd3ec5bc9dcb3527
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-21 22:07:56 +0100

    task is done

--- test_3.c
+++ test_3.c
@@ -6,7 +6,7 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:31:53 by ymomen            #+#    #+#             */
-/*   Updated: 2024/05/20 19:37:50 by ymomen           ###   ########.fr       */
+/*   Updated: 2024/05/21 18:37:09 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ pthread_mutex_t mutix;
 int d[]= {1,2,3,4,5,6,7,8,9,10};
 void *rotine(void *arg)
 {
-	sleep(1);
+	double k = 0.5;
+	sleep(k);
 	int i = *(int *)arg;
 	printf("%d\n",d[i]);
 	free(arg);
@@ -31,23 +32,33 @@ int	main(int ac, char **av)
 {
 	(void)av;
 	pthread_t t1[ac];
-	pthread_mutex_init(&mutix, NULL);
-	int i = 0;
-	while(i < ac)
+	if(fork() != -1)
 	{
-		int *ptr = malloc(4);
-		*ptr = i;
-		if (pthread_create(&t1[i], NULL, &rotine, ptr) != 0)
-			return (1);
-		i++;
+		pthread_mutex_init(&mutix, NULL);
+		int i = 0;
+		while(i < ac)
+		{
+			int *ptr = malloc(4);
+			*ptr = i;
+			if (pthread_create(&t1[i], NULL, &rotine, ptr) != 0)
+				return (1);
+			i++;
+		}
+		i = 0;
+		while(i < ac)
+		{
+			if ((pthread_join(t1[i++], NULL)))
+				return (6);
+		}
+		if(pthread_mutex_destroy(&mutix))
+			return (5);
+		main(ac, av);
+		sleep(5);
 	}
-	i = 0;
-	while(i < ac)
+	else
 	{
-		if ((pthread_join(t1[i++], NULL)))
-			return (6);
+		printf("fork failed\n");
+		return (1);
 	}
-	if(pthread_mutex_destroy(&mutix))
-		return (5);
 	return (0);
 }
