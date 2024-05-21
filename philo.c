5fb45689475bbc68c6219b2d66366d8086f10d7b
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-05-21 18:31:58 +0100

    task is done

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 17:20:07 by ymomen            #+#    #+#             */
-/*   Updated: 2024/05/21 17:20:35 by ymomen           ###   ########.fr       */
+/*   Updated: 2024/05/21 18:01:04 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,6 @@
 void philo(char **av, int ac)
 {
 	int i;
-	int nb;
-
-	i = 1;
-	while (i < ac)
-	{
-		nb = ft_atoi(av[i]);
-		if (nb == -1)
-			error("Error: wrong argument\n");
-		printf("%d\n", nb);
-		i++;
-	}
+	t_philo philo;
+	
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 12:34:23 by ymomen            #+#    #+#             */
-/*   Updated: 2024/05/21 16:48:43 by ymomen           ###   ########.fr       */
+/*   Updated: 2024/05/21 18:24:54 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@
 # define MAX_DOUBLE 922337203685477580
 
 /*STRUCTS*/
+typedef struct s_philo
+{
+	int				nb_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nb_eat;
+	int				*forks;
+	pthread_mutex_t	*forks_mutex;
+	pthread_mutex_t	mutex;
+	pthread_t		*philo;
+}	t_philo;
+typedef struct s_args
+{
+	t_philo	*philo;
+	int		i;
+}	t_args;
 
-	/*FUNCTIONS*/
+/*FUNCTIONS*/
+int		ft_atoi(const char *str);
+void	error(char *str);
 #endif /*PHILO_H*/
